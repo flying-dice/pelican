ed03bf20d870029782d58c0ad3265dff8859c2ff
🎲 <dice-a6db@outlook.com>
2025-01-19 00:12:12 +0000

    fix: add bat file for release

---
+++ release.bat
@@ -0,0 +1,18 @@
+@echo off
+
+:: Delete the target folder if it exists
+if exist target (
+    rmdir /s /q target
+    echo Deleted the target folder.
+) else (
+    echo No target folder found, skipping deletion.
+)
+
+:: Run cargo build with the custom environment variables
+cargo build
+
+:: Run Tests
+lua5.1 ltests/test.lua
+
+:: Run Release
+cargo release --execute --no-publish
