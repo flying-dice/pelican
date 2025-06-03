5f51eb5730aeed836d9a7a68dde2cd31a3909e06
🎲 <dice-a6db@outlook.com>
2025-06-03 23:35:35 +0100

    docs: add examples of jsonrpc server

--- release.bat
+++ release.bat
@@ -1,23 +1,15 @@
 @echo off
 
-:: Delete the target folder if it exists
-if exist target (
-    rmdir /s /q target
-    echo Deleted the target folder.
-) else (
-    echo No target folder found, skipping deletion.
-)
+@REM :: Delete the target folder if it exists
+@REM if exist target (
+@REM     rmdir /s /q target
+@REM     echo Deleted the target folder.
+@REM ) else (
+@REM     echo No target folder found, skipping deletion.
+@REM )
 
 :: Run cargo build with the custom environment variables
 cargo build
-
-:: Run Tests
-cd tstl
-npm install
-npm run test
-npm run test:e2e
-
-cd ..
 
 :: Run Release
 cargo release --execute --no-publish
