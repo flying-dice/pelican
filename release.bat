0441e005e631d7dea76ae865f33fcb2276744be6
🎲 <dice-a6db@outlook.com>
2025-06-26 00:13:31 +0100

    chore: simplify release script by removing debug build and test steps

--- release.bat
+++ release.bat
@@ -1,19 +1,5 @@
 @echo off
 
-:: Run Debug build and tests
-if exist target (
-    rmdir /s /q target
-    echo Deleted the target folder.
-) else (
-    echo No target folder found, skipping deletion.
-)
-
-cargo build
-
-cd tstl
-
-npm i && npm run test && npm run test:e2e
-
 :: Run Release build
 if exist target (
     rmdir /s /q target
